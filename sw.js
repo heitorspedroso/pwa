ce183ba509fda159d6a1799bcdd4228d19d00740
Heitor <heitorspedroso@gmail.com>
2021-02-03 22:56:53 -0300

    version 1.0

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 this.addEventListener('install', function(event) {
     console.log('install' , event)
     event.waitUntil(
-        caches.open('v2').then(function(cache) {
+        caches.open('v3').then(function(cache) {
             return cache.addAll([
                 '/pwa/',
                 '/pwa/index.html',
@@ -18,7 +18,7 @@ this.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(resp) {
             return resp || fetch(event.request).then(function(response) {
-                return caches.open('v2').then(function(cache) {
+                return caches.open('v3').then(function(cache) {
                     cache.put(event.request, response.clone());
                     return response;
                 });
@@ -28,15 +28,21 @@ this.addEventListener('fetch', function(event) {
 });
 
 this.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['v2'];
+    var cacheWhitelist = ['v3'];
     console.log('activate',event)
     event.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
                 if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Delete cache', cacheName);
                     return caches.delete(key);
                 }
             }));
         })
     );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event);
+    clients.openWindow('/');
 });

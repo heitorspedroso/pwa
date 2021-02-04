63bd498c075f9106c3eb4245eee6c6b666ba7874
Heitor <heitorspedroso@gmail.com>
2021-02-03 21:59:53 -0300

    version 1.0

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('sw.js',{scope:'/pwa/'})
         .then(function(reg) {
             // registration worked
             console.log('Registration succeeded. Scope is ' + reg.scope);
---
+++ sw.js
@@ -0,0 +1,11 @@
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/pwa/',
+                '/pwa/index.html',
+                '/pwa/sw.js',
+            ]);
+        })
+    );
+});

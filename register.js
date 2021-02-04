3b07e35060bbe810119d86ce5510b04a36d48994
Heitor <heitorspedroso@gmail.com>
2021-02-03 21:48:39 -0300

    version 1.0

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw-test/sw.js', {scope: '/sw-test/'})
+if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+    navigator.serviceWorker.register('sw.js', {scope: '/'})
         .then(function(reg) {
             // registration worked
             console.log('Registration succeeded. Scope is ' + reg.scope);

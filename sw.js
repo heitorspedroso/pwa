7ef50d345fa0180d4785e1735f6936292653417b
Heitor <heitorspedroso@gmail.com>
2021-02-03 22:04:37 -0300

    version 1.0

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 this.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
+            console.log('install');
             return cache.addAll([
                 '/pwa/',
                 '/pwa/index.html',

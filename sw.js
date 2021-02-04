48b47bbe942f76b805dc19b2d1318de59f0dcff6
Heitor <heitorspedroso@gmail.com>
2021-02-03 22:30:39 -0300

    version 1.0

--- sw.js
+++ sw.js
@@ -15,6 +15,13 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
     console.log('[SW] fetch ' + event.request.url)
     event.respondWith(
-        caches.match(event.request)
+        caches.match(event.request).then(function(resp) {
+            return resp || fetch(event.request).then(function(response) {
+                return caches.open('v1').then(function(cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
     );
 });

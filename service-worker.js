ff180e0ecc1e9c5dee8854f2179be72378760a4b
Heitor <heitorspedroso@gmail.com>
2022-04-27 11:14:56 -0300

    test

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,46 @@
-const cacheName = 'Temporas';
+var CACHE_NAME = 'cachacaria-v1';
+// dev only
+if (typeof files == 'undefined') {
+    var files = [];
+} else {
+    files.push('./');
+}
 
-// Cache all the files to make a PWA
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            // Our application only has two files here index.html and manifest.json
-            // but you can add more such as style.css as your app grows
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json'
-            ]);
+
+self.addEventListener('activate', function(event) {
+    console.log('[SW] Activate');
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (CACHE_NAME.indexOf(cacheName) == -1) {
+                        console.log('[SW] Delete cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
-self.addEventListener('fetch', event => {
+self.addEventListener('install', function(event){
+    console.log('[SW] Install');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return Promise.all(
+                files.map(function(file){
+                    return cache.add(file);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('[SW] fetch ' + event.request.url)
     event.respondWith(
-        caches.open(cacheName)
-            .then(cache => cache.match(event.request, { ignoreSearch: true }))
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request.clone());
+        })
     );
 });

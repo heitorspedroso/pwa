a128c4fab9cc190aae9bd4c11511c2357d423f7a
Heitor <heitorspedroso@gmail.com>
2022-04-27 11:25:45 -0300

    test

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,19 @@
-var CACHE_NAME = 'cachacaria-v1';
-// dev only
-if (typeof files == 'undefined') {
-    var files = [];
-} else {
-    files.push('./');
-}
+let cacheVersion = 1
+let cacheName = "web-workr-cache-"+cacheVersion
+const pageToSave = "offline.html"
+
+this.addEventListener('install', event => {
+    console.log("Installing service worker");
+})
+
+this.addEventListener('activate', event => {
+    console.log("Activate service worker");
+})
 
 
-self.addEventListener('activate', function(event) {
-    console.log('[SW] Activate');
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (CACHE_NAME.indexOf(cacheName) == -1) {
-                        console.log('[SW] Delete cache:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('install', function(event){
-    console.log('[SW] Install');
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return Promise.all(
-                files.map(function(file){
-                    return cache.add(file);
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log('[SW] fetch ' + event.request.url)
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            return response || fetch(event.request.clone());
-        })
-    );
-});
+this.addEventListener('fetch', event => {
+    console.log("Fetching with service worker");
+    if(event.request.mode === 'navigate'){
+        console.log(event.request);
+    }
+})

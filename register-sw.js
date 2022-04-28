65137845d870c300273b1bfa92c5fa4794c52e13
Heitor <heitorspedroso@gmail.com>
2022-04-27 23:05:24 -0300

    1.0.0

--- register-sw.js
+++ register-sw.js
@@ -1,9 +1,14 @@
-window.addEventListener('load', () => {
-    navigator.serviceWorker
-        .register('service-worker.js')
-        .then(_ => console.log('Registered service worker'))
-        .catch(e => console.log('Error registering: ',err));
-});
+if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+    navigator.serviceWorker.register('files/service-worker.js', {
+        scope: './'
+    }).then(function(registration) {
+        if (typeof registration.update == 'function') {
+            registration.update();
+        }
+    }).catch(function(e) {
+        console.error('Error during service worker registration:', e);
+    });
+}
 
 window.addEventListener('online', () => {
     window.location.reload();
